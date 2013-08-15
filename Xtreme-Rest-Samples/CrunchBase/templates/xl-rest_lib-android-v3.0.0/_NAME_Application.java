0f3c22993cea306248cd9081be28cb62da186d41
Joshua Winters <joshua@xtremelabs.com>
2013-08-15 19:24:48 -0400

    JW - updated templates to match the Database.init removal

--- Xtreme-Rest-Samples/CrunchBase/templates/xl-rest_lib-android-v3.0.0/_NAME_Application.java
+++ Xtreme-Rest-Samples/CrunchBase/templates/xl-rest_lib-android-v3.0.0/_NAME_Application.java
@@ -2,17 +2,11 @@ package ${base_package}.application;
 
 import android.app.Application;
 
-import com.xtreme.rest.providers.Database;
-
 public class ${project_name}Application extends Application {
 	
-	private static final int DATABASE_VERSION = 1;
-	private static final String DATABASE_NAME = "${common.underscore(project_name)}.db";
-
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		
-		Database.init(DATABASE_NAME, DATABASE_VERSION);
 	}
 }

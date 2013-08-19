87f5f8293d130eec4c04ff3870318cfaf6c196b1
Joshua Winters <joshua@xtremelabs.com>
2013-08-19 11:03:03 -0400

    JW - shortened variable names in the ServiceError class

--- Xtreme-Rest-Service/src/com/xtreme/rest/service/ServiceError.java
+++ Xtreme-Rest-Service/src/com/xtreme/rest/service/ServiceError.java
@@ -15,65 +15,66 @@ public class ServiceError implements Parcelable {
 	}
 
 	public static class Messages {		
-		public static final String UNKNOWN = "Unknown Error";
+		public static final String UNKNOWN = "An unknown error occured.";
 	}
 
-	private final int mErrorCode;
-	private final String mErrorMessage;
-	private final String mErrorType;
+	private final int mCode;
+	private final String mMessage;
+	private final String mType;
 
 	/**
 	 * Creates a {@link ServiceError} with the provided info.
 	 * 
-	 * @param errorCode The associated code
-	 * @param errorType The associated type
-	 * @param errorMessage The associated message
+	 * @param code The associated code
+	 * @param type The associated type
+	 * @param message The associated message
 	 */
-	public ServiceError(final int errorCode, final String errorType, final String errorMessage) {
-		mErrorCode = errorCode;
-		mErrorMessage = errorMessage;
-		mErrorType = errorType;
+	public ServiceError(final int code, final String type, final String message) {
+		mCode = code;
+		mMessage = message;
+		mType = type;
 	}
 	
 	/**
 	 * Creates a {@link ServiceError} with a null error type.
 	 * 
-	 * @param errorCode The code associated with this {@link ServiceError}
-	 * @param errorMessage The message associated with this {@link ServiceError}
+	 * @param code The code associated with this {@link ServiceError}
+	 * @param message The message associated with this {@link ServiceError}
 	 */
-	public ServiceError(final int errorCode, final String errorMessage) {
-		this(errorCode, null, errorMessage);
+	public ServiceError(final int code, final String message) {
+		this(code, null, message);
 	}
 	
 	/**
 	 * Creates a {@link ServiceError} with {@link Codes#UNKNOWN} as the error code and a null error type.
 	 * 
-	 * @param errorMessage The message associated with this {@link ServiceError}
+	 * @param message The message associated with this {@link ServiceError}
 	 */
-	public ServiceError(final String errorMessage) {
-		this(Codes.UNKNOWN, errorMessage);
+	public ServiceError(final String message) {
+		this(Codes.UNKNOWN, message);
 	}
 
 	/**
-	 * A convenience constructor that takes creates a formatted message from the exception and then calls {@link #ServiceError(String)}.
+	 * A convenience constructor that takes an {@link Exception} and then calls {@link #ServiceError(String)} 
+	 * with the localized message from the exception.
 	 * 
-	 * @param e The exception whose info is used for the formatted message.
+	 * @param exception The exception whose localized message to use.
 	 */
-	public ServiceError(final Exception e) {
-		this(String.format("Exception class: %s, Exception message: %s",  e.getClass().getName(), e.getLocalizedMessage()));
+	public ServiceError(final Exception exception) {
+		this(exception.getLocalizedMessage());
 	}
 
 	public ServiceError(final Parcel in) {
-		mErrorCode = in.readInt();
-		mErrorMessage = in.readString();
-		mErrorType = in.readString();
+		mCode = in.readInt();
+		mMessage = in.readString();
+		mType = in.readString();
 	}
 
 	@Override
 	public void writeToParcel(final Parcel dest, final int flags) {
-		dest.writeInt(mErrorCode);
-		dest.writeString(mErrorMessage);
-		dest.writeString(mErrorType);
+		dest.writeInt(mCode);
+		dest.writeString(mMessage);
+		dest.writeString(mType);
 	}
 	
 	@Override
@@ -82,20 +83,20 @@ public class ServiceError implements Parcelable {
 	}
 
 	public String getMessage() {
-		return mErrorMessage;
+		return mMessage;
 	}
 
 	public int getCode() {
-		return mErrorCode;
+		return mCode;
 	}
 	
 	public String getType() {
-		return mErrorType;
+		return mType;
 	}
 	
 	@Override
 	public String toString() {
-		return String.format(Locale.getDefault(), "[%d] %s", mErrorCode, mErrorMessage);
+		return String.format(Locale.getDefault(), "[%d] %s", mCode, mMessage);
 	}
 	
 	public static final Parcelable.Creator<ServiceError> CREATOR = new Parcelable.Creator<ServiceError>() {

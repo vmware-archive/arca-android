0f29b06a5a512c62070de2ece65ae466f5104f00
Joshua Winters <joshua@xtremelabs.com>
2014-02-21 17:52:09 -0500

    JW - removed old api key files

--- Xtreme-Rest-Samples/CrunchBase/src/com/crunchbase/app/application/CrunchBaseApi.java
+++
@@ -1,7 +0,0 @@
-package com.crunchbase.app.application;
-
-public class CrunchBaseApi {
-
-	public static final String KEY = "YOU_NEED_A_CRUNCH_BASE_API_KEY";
-
-}
--- Xtreme-Rest-Samples/RottenTomatoes/src/com/rottentomatoes/app/application/RottenTomatoesApi.java
+++
@@ -1,7 +0,0 @@
-package com.rottentomatoes.app.application;
-
-public class RottenTomatoesApi {
-
-	public static final String KEY = "YOU_NEED_A_ROTTEN_TOMATOES_API_KEY";
-
-}

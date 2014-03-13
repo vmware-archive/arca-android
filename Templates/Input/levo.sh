7a5f5708936c1c2dbca3be7f3af321ccecf3f3de
Joshua Winters <joshua@xtremelabs.com>
2014-03-13 12:53:30 -0400

    JW - updated templates with better naming conventions

--- Templates/Input/levo.sh
+++ Templates/Input/levo.sh
@@ -1,5 +1,5 @@
 export PATH=$PATH:~/go/bin; cd ../Output;
 
-#levo -f -k com.template -p Template -t ../Input/android -m "User id:long name:string age:int" -m "Post id:long text:string user_id:long"
-levo -f -k com.template -p Template -t ../Input/android_sync -m "User id:long name:string age:int" -m "Post id:long text:string user_id:long"
-levo -f -k com.template -p Template -t ../Input/rails -m "User id:long name:string age:int" -m "Post id:long text:string user_id:long"
+#levo -f -k com.chat -p Chat -t ../Input/android -m "User id:long name:string age:int" -m "Post id:long text:string user_id:long"
+levo -f -k com.chat -p Chat -t ../Input/android_sync -m "User id:long name:string age:int" -m "Post id:long text:string user_id:long"
+levo -f -k com.chat -p Chat -t ../Input/rails -m "User id:long name:string age:int" -m "Post id:long text:string user_id:long"

eceeae1b4a547df18935c6620284dba7c8ec5a84
Joshua Winters <joshua@xtremelabs.com>
2014-03-25 16:14:38 -0400

    JW - updated templates to take advantage of the new static templating system

--- Templates/Input/levo.sh
+++ Templates/Input/levo.sh
@@ -1,5 +1,4 @@
 export PATH=$PATH:~/go/bin; cd ../Output;
 
-#levo -f -k com.chat -p Chat -t ../Input/android -m "User id:long name:string age:int" -m "Post id:long text:string user_id:long"
-levo -f -k com.chat -p Chat -t ../Input/android_sync -m "User id:long name:string age:int" -m "Post id:long text:string user_id:long"
+levo -f -k com.chat -p Chat -t ../Input/android -m "Post id:long text:string user_id:long" -m "User id:long name:string age:int"
 levo -f -k com.chat -p Chat -t ../Input/rails -m "User id:long name:string age:int" -m "Post id:long text:string user_id:long"
